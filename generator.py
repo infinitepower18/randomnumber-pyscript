d22558c22a99fa224dc4fa7306b541cdec90e778
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-05-28 16:52:05 +0100

    generate number without refreshing

--- generator.py
+++ generator.py
@@ -1,2 +1,10 @@
 import random
-print("Your random number is " +str(random.randint(1,100)))
+from js import document
+from pyodide import create_proxy
+
+def generate(event):
+        s = "Your random number is " + str(random.randint(1,100))
+
+        document.getElementById("output").innerHTML = s
+
+document.getElementById("generate").addEventListener("click", create_proxy(generate))
